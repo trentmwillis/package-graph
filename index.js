cf8ea5222ee4672b8eee71fc576e2b018a6516a0
Trent Willis <trentmwillis@gmail.com>
2017-02-10 21:40:28 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require('fs-extra');
+const findUp = require('find-up');
+const path = require('path');
+
+const nodeList = Object.create(null);
+
+function findNearestPackage(cwd) {
+  let packagePath = findUp.sync('package.json', { cwd });
+  return fs.readJsonSync(packagePath);
+}
+
+function initNode(package, options = {}) {
+  let id = `${package.name}@${package.version}`;
+
+  if (nodeList[id]) {
+    return nodeList[id];
+  }
+
+  let node = nodeList[id] = {
+    id,
+    dependencies: []
+  };
+
+  let deps = Object.keys(package.dependencies || {});
+
+  if (options.includeDevDeps) {
+    let devDeps = Object.keys(package.devDependencies || {});
+    deps.push(...devDeps);
+  }
+
+  if (options.includeEmberAddons) {
+    // Do stuff for ember addons
+  }
+
+  for (let i = 0; i < deps.length; i++) {
+    let dep = deps[i];
+    let depPath = path.dirname(require.resolve(dep));
+    let depPackage = findNearestPackage(depPath);
+
+    let depNode = initNode(depPackage);
+
+    node.dependencies.push(depNode.id);
+  }
+
+  return node;
+}
+
+function createGraph() {
+  let startingPackage = findNearestPackage(process.cwd());
+  initNode(startingPackage, {
+    includeDevDeps: true,
+    includeEmberAddons: true
+  });
+  return nodeList;
+}
+
+console.log(JSON.stringify(createGraph(), null, 2));
