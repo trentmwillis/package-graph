39b84c221225a5b9c583cc5b9edc9f78e0008b47
Trent Willis <trentmwillis@gmail.com>
2017-02-11 20:36:24 -0800

    Make visualization more interactive

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const createPackageGraph = require('./lib/create-package-graph');
 
 fs.mkdirpSync('dist');
 fs.writeJsonSync('dist/package.graph.json', createPackageGraph());
+fs.copySync('node_modules/d3/build/d3.min.js', 'dist/d3.min.js');
